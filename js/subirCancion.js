70db8d4307712568b52ffd24a7a0fe3c45f98c28
jgarriuni <jgarri04@ucm.es>
2017-04-24 12:13:53 +0200

    nonscrip en miperfil subircancion login, js para subircancion mejorado

--- js/subirCancion.js
+++ js/subirCancion.js
@@ -37,3 +37,68 @@ function validarArtista() {
         return true;
     }
 }
+
+function validarImagen(imagen){
+
+/*
+ID_Imagen
+ID_Error_Imagen
+*/
+
+    "use strict";
+
+    if(imagen != null){
+
+        var uploadImg = imagen.files[0];
+        if(!(/\.(jpg|png)$/i).test(uploadImg.name)){
+            document.getElementById("ID_Error_Imagen").style.display = "block";
+            document.getElementById("ID_Error_Imagen").innerHTML = "La extension del archivo no se soporta. Solo jpg y png";
+            return false;
+        }
+        else{
+            document.getElementById("ID_Error_Imagen").style.display = "block";
+            return true;
+        }
+    }
+    else{
+        document.getElementById("ID_Error_Imagen").style.display = "none";
+        return true;
+    }
+}
+
+function validarArchivoCancion(cancion){
+    "use strict";
+
+    if(cancion != null){
+
+        var uploadCancion = cancion.files[0];
+        if(!(/\.(mp3|mp4|wav)$/i).test(uploadCancion.name)){
+            document.getElementById("ID_Error_Archivo_Cancion").style.display = "block";
+            document.getElementById("ID_Error_Archivo_Cancion").innerHTML = "La extension de la cancion no se soporta. Solo mp3, mp4 y wav";
+            return false;
+        }
+        else{
+            document.getElementById("ID_Error_Archivo_Cancion").style.display = "none";
+            return true;
+        }
+    }
+    else{
+        document.getElementById("ID_Error_Archivo_Cancion").style.display = "block";
+        document.getElementById("ID_Error_Archivo_Cancion").innerHTML = "No puedes dejar vacio el campo cancion";
+        return false;
+    }
+}
+
+function validarFormCancion(){
+    "use strict";
+
+    var ok = (validarCancion() & validarArtista() & validarImagen() & validarArchivoCancion());
+
+    if(ok == 0){
+        return false;
+    }
+    else{
+        return true;
+    }
+
+}
