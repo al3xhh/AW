bdcf0e1dc586e53b963cba2b6ee4b71cdb57bc33
jose <jgarri04@ucm.es>
2017-05-20 19:51:13 +0200

    Solucionando subir archivos en linux

--- php/modelo.php
+++ php/modelo.php
@@ -14,7 +14,7 @@ function obtenerArray($stmt, &$array, $col1_n, $col2_n, $col3_n) {
 
 //Función para conectar con la base de datos.
 function conectar() {
-   return new mysqli('127.0.0.1', 'root', '', 'WebMusic');
+   return new mysqli('127.0.0.1', 'root', 'joseubuntu', 'WebMusic');
 }
 
 //Función para limpiar la entrada de cualquier caracter raro.
