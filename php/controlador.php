2ed66c69d532de7828d9e8ff7b320439d5d916e3
Christian7007 <christig@ucm.es>
2017-05-24 18:39:29 +0200

    ya añade la informcion del usaurio premiun a la BD

--- php/controlador.php
+++ php/controlador.php
@@ -1,92 +1,98 @@
 <?php
 
-   require_once("modelo.php");
+require_once("modelo.php");
 
-   function validar_entrada($dato) {
-      return validarEntrada($dato);
+function validar_entrada($dato) {
+   return validarEntrada($dato);
+}
+
+function existe_usuario($usuario) {
+   return existeUsuario($usuario);
+}
+
+function existe_correo($correo) {
+   return existeCorreo($correo);
+}
+
+function registra_usuario($usuario, $correo, $contrasenya) {
+   return registraUsuario($usuario, $correo, $contrasenya);
+}
+
+function tus_novedades($usuario) {
+   return tusNovedades($usuario);
+}
+
+function tus_top($usuario) {
+   return tusTop($usuario);
+}
+
+function tus_top_social($usuario) {
+   return tusTopSocial($usuario);
+}
+
+function info_cancion($titulo, $autor) {
+
+   return getInfoCancion($titulo, $autor);
+}
+
+function aniadir_premium($usuario, $n_cuenta, $cvv, $fecha_caducidad_cuenta, $titular, $n_meses, $fecha_caducidad_premium, $pass) {
+   if (existeUsuario($usuario)){
+      if (autenticarUsuario($usuario, $pass))
+         return aniadirPremium($usuario, $n_cuenta, $cvv, $fecha_caducidad_cuenta, $titular, $n_meses, $fecha_caducidad_premium);
+      else 
+         return false;
    }
+   else
+      return false;
+}
 
-   function existe_usuario($usuario) {
-      return existeUsuario($usuario);
-   }
+function autenticar($usuario, $pass) {
+   return autenticarUsuario($usuario, $pass);
+}
 
-   function existe_correo($correo) {
-      return existeCorreo($correo);
-   }
+function obtener_gustos_musicales($usuario) {
+   return obtenerGustosMusicales($usuario);
+}
 
-   function registra_usuario($usuario, $correo, $contrasenya) {
-      return registraUsuario($usuario, $correo, $contrasenya);
-   }
+function obtener_informacion_usuario($usuario) {
+   return obtenerInformacionUsuario($usuario);
+}
 
-   function tus_novedades($usuario) {
-      return tusNovedades($usuario);
-   }
+function obtener_descripcion_usuario($usuario) {
+   return obtenerDescripcionUsuario($usuario);
+}
 
-   function tus_top($usuario) {
-      return tusTop($usuario);
-   }
+function obtener_seguidores($usuario) {
+   return obtenerSeguidores($usuario);
+}
 
-   function tus_top_social($usuario) {
-      return tusTopSocial($usuario);
-   }
+function obtener_seguidos($usuario) {
+   return obtenerSeguidos($usuario);
+}
 
-   function info_cancion($titulo, $autor) {
+function obtener_canciones_usuario($usuario) {
+   return obtenerCancionesUsuario($usuario);
+}
 
-      return getInfoCancion($titulo, $autor);
-   }
+function sigue_a($seguidor, $seguido) {
+   return sigueA($seguidor, $seguido);
+}
 
-   function aniadir_premium($usuario, $n_cuenta, $cvv, $fecha_caducidad_cuenta, $titular, $n_meses, $fecha_caducidad_premium) {
+function obtener_top() {
+   return obtenerTop();
+}
 
-      return aniadirPremium($usuario, $n_cuenta, $cvv, $fecha_caducidad_cuenta, $titular, $n_meses, $fecha_caducidad_premium);
-   }
+function obtener_novedades() {
+   return obtenerNovedades();
+}
 
-   function autenticar($usuario, $pass) {
-      return autenticarUsuario($usuario, $pass);
-   }
+function obtener_listas_reproduccion_usuario($usuario){
+   return obtenerListasReproduccionUsuario($usuario);
+}
 
-   function obtener_gustos_musicales($usuario) {
-      return obtenerGustosMusicales($usuario);
-   }
+function obtener_info_comentarios_cancion($cancion, $usuario){
 
-   function obtener_informacion_usuario($usuario) {
-      return obtenerInformacionUsuario($usuario);
-   }
-
-   function obtener_descripcion_usuario($usuario) {
-      return obtenerDescripcionUsuario($usuario);
-   }
-
-   function obtener_seguidores($usuario) {
-      return obtenerSeguidores($usuario);
-   }
-
-   function obtener_seguidos($usuario) {
-      return obtenerSeguidos($usuario);
-   }
-
-   function obtener_canciones_usuario($usuario) {
-      return obtenerCancionesUsuario($usuario);
-   }
-
-   function sigue_a($seguidor, $seguido) {
-      return sigueA($seguidor, $seguido);
-   }
-
-   function obtener_top() {
-      return obtenerTop();
-   }
-
-   function obtener_novedades() {
-      return obtenerNovedades();
-   }
-
-   function obtener_listas_reproduccion_usuario($usuario){
-      return obtenerListasReproduccionUsuario($usuario);
-   }
-
-   function obtener_info_comentarios_cancion($cancion, $usuario){
-
-      return obtenerInfoComentariosCancion($cancion, $usuario);
-   }
+   return obtenerInfoComentariosCancion($cancion, $usuario);
+}
 
 ?>
--- php/modelo.php
+++ php/modelo.php
@@ -14,7 +14,7 @@ function obtenerArray($stmt, &$array, $col1_n, $col2_n, $col3_n) {
 
 //Función para conectar con la base de datos.
 function conectar() {
-   return new mysqli('127.0.0.1', 'webmusic', 'webmusic', 'WebMusic');
+   return new mysqli('127.0.0.1', 'root', '', 'WebMusic');
 }
 
 //TODO quitarla de aquí y corregir todas las llamadas que se hagan
@@ -545,6 +545,7 @@ function aniadirPremium($usuario, $n_cuenta, $cvv, $fecha_caducidad_cuenta, $tit
    $stmt->execute();
    $stmt->close();
    desconectar($mysqli);
+   return true;
 }
 
 function desconectar($conexion){
